7b4e57a4220ee24c06ab08a8fb6c2045adaa2df3
Mercy <mercycherotich757@gmail.com>
2022-04-06 18:57:35 +0300

    create a profile model

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    bio = models.TextField(max_length=500, default="Bio", blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    contact = models.EmailField(max_length=100, blank=True)
